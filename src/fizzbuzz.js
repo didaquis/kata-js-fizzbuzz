1f1b9343c2aae9c64cdfc3dfed4e1e69eb382e08
Dídac García <didaquis@gmail.com>
2018-08-01 19:39:05 +0200

    Writing first test

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,8 @@
-function fizzbuzz() {
-
+function fizzbuzz(number) {
+	if (number < 1 || number === undefined || typeof number !== 'number' || number > 100) {
+		throw new Error('Invalid argument');
+	}
+	return number;
 }
 
 module.exports = fizzbuzz;
--- test/test.js
+++ test/test.js
@@ -1,2 +1,26 @@
 const fizzbuzz = require('../src/fizzbuzz.js');
 
+describe('Testing FizzBuzz', () => {
+
+	test('Should throw error', () => {
+		expect( () => { fizzbuzz(0); } ).toThrow();
+		expect( () => { fizzbuzz(101); } ).toThrow();
+		expect( () => { fizzbuzz(); } ).toThrow();
+		expect( () => { fizzbuzz('foo'); } ).toThrow();
+		expect( () => { fizzbuzz({}); } ).toThrow();
+		expect( () => { fizzbuzz([]); } ).toThrow();
+	});
+
+	test('Should return number 1', () => {
+		expect(fizzbuzz(1)).toBe(1);
+		expect(fizzbuzz(1)).toEqual(1);
+		expect(typeof fizzbuzz(1)).toEqual('number');
+	});
+
+	test('Should return number 2', () => {
+		expect(fizzbuzz(2)).toBe(2);
+		expect(fizzbuzz(2)).toEqual(2);
+		expect(typeof fizzbuzz(2)).toEqual('number');
+	});
+
+});
