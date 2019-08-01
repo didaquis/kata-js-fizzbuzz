2a557f48fbaef6bc7c8bca517c7ce49d1a88b59b
Dídac García <didaquis@gmail.com>
2019-08-01 23:34:29 +0200

    Refactored code & test

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,32 +1,24 @@
 function fizzbuzz (number) {
-	if (!isIntegerBetweenOneAndOneHundred(number)) {
-		throw new Error('Invalid argument');
-	}
 
-	if (isMultiple(number, 3) && isMultiple(number, 5)) {
-		return 'FizzBuzz';
-	} else if (isMultiple(number, 3)) {
-		return 'Fizz';
-	} else if (isMultiple(number, 5)) {
-		return 'Buzz';
-	}
+	if (!Number.isInteger(number)) throw new Error('Invalid argument');
+	if (!isIntegerInRange(number, 1, 100)) throw new Error('Invalid argument');
 
-	return number;
+	let result = '';
+	if (isMultiple(number, 3)) {
+		result += 'Fizz';
+	}
+	if (isMultiple(number, 5)) {
+		result += 'Buzz';
+	}
+	return result || number;
 }
 
-
-function isIntegerBetweenOneAndOneHundred (number) {
-	if (number < 1 || number === undefined || typeof number !== 'number' || number > 100 || isNaN(number)) {
-		return false;
-	}
-	return true;
+function isIntegerInRange (number, min, max) {
+	return (number >= min && number <= max);
 }
 
 function isMultiple (number, multiple) {
-	if (number % multiple === 0) {
-		return true;
-	}
-	return false;
+	return number % multiple === 0;
 }
 
 module.exports = fizzbuzz;
--- test/test.js
+++ test/test.js
@@ -7,131 +7,48 @@ describe('Testing FizzBuzz', () => {
 	});
 
 	test('Should throw error if argument is not valid', () => {
-		expect( () => { fizzbuzz(0); } ).toThrow();
-		expect( () => { fizzbuzz(101); } ).toThrow();
-		expect( () => { fizzbuzz(-5); } ).toThrow();
-		expect( () => { fizzbuzz(); } ).toThrow();
-		expect( () => { fizzbuzz('foo'); } ).toThrow();
-		expect( () => { fizzbuzz({}); } ).toThrow();
-		expect( () => { fizzbuzz([]); } ).toThrow();
-		expect( () => { fizzbuzz(null); } ).toThrow();
-		expect( () => { fizzbuzz(undefined); } ).toThrow();
-		expect( () => { fizzbuzz(NaN); } ).toThrow();
-		expect( () => { fizzbuzz(() => {}); } ).toThrow();
+		const noValidParameters = [ 0, 101, -5, 'foo', {}, [], null, undefined, NaN, () => {} ];
+		noValidParameters.forEach((n) => {
+			expect( () => { fizzbuzz(n); } ).toThrow();
+		});
 	});
 
 	test('Should throw error with detailed error text', () => {
-		expect( () => { fizzbuzz(0); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz(101); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz(-5); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz(); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz('foo'); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz({}); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz([]); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz(null); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz(undefined); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz(NaN); } ).toThrow('Invalid argument');
-		expect( () => { fizzbuzz(() => {}); } ).toThrow('Invalid argument');
-	});
-
-	test('Should return number 1', () => {
-		expect(fizzbuzz(1)).toBe(1);
-		expect(fizzbuzz(1)).toEqual(1);
-		expect(typeof fizzbuzz(1)).toEqual('number');
-	});
-
-	test('Should return number 2', () => {
-		expect(fizzbuzz(2)).toBe(2);
-		expect(fizzbuzz(2)).toEqual(2);
-		expect(typeof fizzbuzz(2)).toEqual('number');
-	});
-
-	test('Should return number 77', () => {
-		expect(fizzbuzz(77)).toBe(77);
-		expect(fizzbuzz(77)).toEqual(77);
-		expect(typeof fizzbuzz(77)).toEqual('number');
+		const noValidParameters = [ 0, 101, -5, 'foo', {}, [], null, undefined, NaN, () => {} ];
+		noValidParameters.forEach((n) => {
+			expect( () => { fizzbuzz(n); } ).toThrow('Invalid argument');
+		});
 	});
 
 	test('Should return number', () => {
-		expect(typeof fizzbuzz(4)).toEqual('number');
-		expect(typeof fizzbuzz(7)).toEqual('number');
-		expect(typeof fizzbuzz(8)).toEqual('number');
-		expect(typeof fizzbuzz(11)).toEqual('number');
-		expect(typeof fizzbuzz(13)).toEqual('number');
-		expect(typeof fizzbuzz(14)).toEqual('number');
-		expect(typeof fizzbuzz(16)).toEqual('number');
-		expect(typeof fizzbuzz(17)).toEqual('number');
-		expect(typeof fizzbuzz(19)).toEqual('number');
-	});
-
-	test('Should return text', () => {
-		expect(typeof fizzbuzz(3)).toEqual('string');
-		expect(typeof fizzbuzz(5)).toEqual('string');
-		expect(typeof fizzbuzz(6)).toEqual('string');
-		expect(typeof fizzbuzz(9)).toEqual('string');
-		expect(typeof fizzbuzz(10)).toEqual('string');
-		expect(typeof fizzbuzz(12)).toEqual('string');
-		expect(typeof fizzbuzz(15)).toEqual('string');
-		expect(typeof fizzbuzz(18)).toEqual('string');
-		expect(typeof fizzbuzz(20)).toEqual('string');
+		const numbers = [ 1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19, 77 ];
+		numbers.forEach((n) => {
+			expect(fizzbuzz(n)).toEqual(n);
+			expect(Number.isInteger(fizzbuzz(n))).toBe(true);
+		});
 	});
 
 	test('Should return Fizz', () => {
-		expect(fizzbuzz(3)).toBe('Fizz');
-		expect(fizzbuzz(3)).toEqual('Fizz');
-		expect(typeof fizzbuzz(3)).toEqual('string');
-
-		expect(fizzbuzz(6)).toBe('Fizz');
-		expect(fizzbuzz(6)).toEqual('Fizz');
-		expect(typeof fizzbuzz(6)).toEqual('string');
-
-		expect(fizzbuzz(9)).toBe('Fizz');
-		expect(fizzbuzz(9)).toEqual('Fizz');
-		expect(typeof fizzbuzz(9)).toEqual('string');
-
-		expect(fizzbuzz(66)).toBe('Fizz');
-		expect(fizzbuzz(66)).toEqual('Fizz');
-		expect(typeof fizzbuzz(66)).toEqual('string');
+		const numbers = [ 3, 6, 9, 66 ];
+		numbers.forEach((n) => {
+			expect(fizzbuzz(n)).toEqual('Fizz');
+			expect(typeof fizzbuzz(n)).toEqual('string');
+		});
 	});
 
 	test('Should return Buzz', () => {
-		expect(fizzbuzz(5)).toBe('Buzz');
-		expect(fizzbuzz(5)).toEqual('Buzz');
-		expect(typeof fizzbuzz(5)).toEqual('string');
-
-		expect(fizzbuzz(10)).toBe('Buzz');
-		expect(fizzbuzz(10)).toEqual('Buzz');
-		expect(typeof fizzbuzz(10)).toEqual('string');
-
-		expect(fizzbuzz(20)).toBe('Buzz');
-		expect(fizzbuzz(20)).toEqual('Buzz');
-		expect(typeof fizzbuzz(20)).toEqual('string');
-
-		expect(fizzbuzz(80)).toBe('Buzz');
-		expect(fizzbuzz(80)).toEqual('Buzz');
-		expect(typeof fizzbuzz(80)).toEqual('string');
-
-		expect(fizzbuzz(100)).toBe('Buzz');
-		expect(fizzbuzz(100)).toEqual('Buzz');
-		expect(typeof fizzbuzz(100)).toEqual('string');
+		const numbers = [ 5, 10, 20, 80, 100 ];
+		numbers.forEach((n) => {
+			expect(fizzbuzz(n)).toEqual('Buzz');
+			expect(typeof fizzbuzz(n)).toEqual('string');
+		});
 	});
 
 	test('Should return FizzBuzz', () => {
-		expect(fizzbuzz(15)).toBe('FizzBuzz');
-		expect(fizzbuzz(15)).toEqual('FizzBuzz');
-		expect(typeof fizzbuzz(15)).toEqual('string');
-
-		expect(fizzbuzz(30)).toBe('FizzBuzz');
-		expect(fizzbuzz(30)).toEqual('FizzBuzz');
-		expect(typeof fizzbuzz(30)).toEqual('string');
-
-		expect(fizzbuzz(60)).toBe('FizzBuzz');
-		expect(fizzbuzz(60)).toEqual('FizzBuzz');
-		expect(typeof fizzbuzz(60)).toEqual('string');
-
-		expect(fizzbuzz(90)).toBe('FizzBuzz');
-		expect(fizzbuzz(90)).toEqual('FizzBuzz');
-		expect(typeof fizzbuzz(90)).toEqual('string');
+		const numbers = [ 15, 30, 60, 90 ];
+		numbers.forEach((n) => {
+			expect(fizzbuzz(n)).toEqual('FizzBuzz');
+			expect(typeof fizzbuzz(n)).toEqual('string');
+		});
 	});
-
 });
